08f88d3144c43326a0afec45d4b7dccc1be2c59d
ssanaga <sowmya.sanagapalli@gmail.com>
2024-01-31 15:00:35 -0600

    Expand cors

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(morgan("dev"));
 app.use(helmet());
 
 const corsOptions = {
-    origin: ['https://pool360.vercel.app', 'https://ecommercepool360.vercel.app', 'http://localhost:3000', 'http://localhost:5000'], // Add your origins here
+    origin: [authConfig.cors], // Add your origins here
     optionsSuccessStatus: 200
 };
 
